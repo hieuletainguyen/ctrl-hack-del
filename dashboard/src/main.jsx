a3c95993fd548f84f33bbdd3869a33bb9e30954b
7up11 <83753733+7up11@users.noreply.github.com>
2024-11-09 15:34:20 -0500

    Set up react router

--- dashboard/src/main.jsx
+++ dashboard/src/main.jsx
@@ -1,10 +1,25 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import {createBrowserRouter, RouterProvider} from "react-router-dom"
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <div>Index</div>
+    },
+    {
+        path: "/signup",
+        element: <div>Sign up</div>,
+    },
+    {
+        path: "/signin",
+        element: <div>Sign in</div>
+    }
+])
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <App />
+        <RouterProvider router={router} />
     </StrictMode>,
 )
