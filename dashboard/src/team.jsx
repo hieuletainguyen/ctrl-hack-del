c9b070edc160cc364353ef73ccc3625abe522ce8
Agiat Ikazinat <151879446+hieuletainguyen@users.noreply.github.com>
2024-11-10 08:08:51 -0500

    starting team

--- dashboard/src/team.jsx
+++ dashboard/src/team.jsx
@@ -13,7 +13,55 @@ const List = ({nurses}) => {
 }
 
 const Profile = ({nurse}) => {
-    return <></>
+    const [skills, setSkills] = useState([]);
+    const [edit, setEdit] = useState(false);
+    const [fullName, setFullName] = useState(nurse.fullName);
+    const [selectedSkills, setSelectedSkills] = useState([]);
+    const [nurseSkills, setNurseSkills] = useState(nurse.skills);
+
+    useEffect(() => {
+        const url = `/skills/get-skill`;
+        authenticatedFetch(url).then(res => setSkills(res.data));
+    }, []);
+
+    const handleUpdateNurse = () => {
+        setEdit(false)
+        const url = `/nurses/update-nurse/${nurse.id}`;
+        authenticatedFetch(url, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({fullName: fullName, skills: skills})
+        }).then(res => {
+            if (res?.accepted) setEdit(false);
+        });
+    }
+
+    return <>
+        {
+            edit ?
+            <div>
+                <input type="text" value={fullName} onChange={(e) => setFullName(e.target.value)} />
+                <select multiple value={selectedSkills} onChange={(e) => {
+                    const selected = Array.from(e.target.selectedOptions, option => option.value);
+                    setSelectedSkills(selected);
+                }}>
+                    {skills.filter(s => !nurseSkills.includes(s))
+                        .map(s => <option key={s} value={s}>{s}</option>)}
+                </select>
+                <button onClick={() => {handleUpdateNurse()}}>Save</button>
+            </div> :
+            <>
+                <h1>{nurse.fullName}</h1>
+                <ol>
+                    {nurse.skills.map(s => <li key={s}>{s}</li>)}
+                </ol>
+                <button onClick={() => setEdit(true)}>Edit</button>
+            </>
+        }
+
+    </>
 }
 
 export default () => {
